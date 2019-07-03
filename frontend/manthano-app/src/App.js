84d660759a37d0b43693ab44bede0ba28e6f68f9
Jonathan Gruber <gruberjonathan@gmail.com>
2019-07-03 22:14:28 +0200

    Workaround for setting state in onChange handler

--- frontend/manthano-app/src/App.js
+++ frontend/manthano-app/src/App.js
@@ -8,31 +8,31 @@ const kNearNeigh = {
   name: 'Blabla',
   category: 'Machine Learning',
   block: {
-    init: function() {
+    init: function () {
       this.jsonInit({
-      message0: Blockly.Msg['TEXT_PRINT_TITLE'],
-      args0: [
-        {
-          type: "input_value",
-          name: "TEXT"
-        }
-      ],
-      style: "text_blocks",
-      tooltip: Blockly.Msg['TEXT_PRINT_TOOLTIP']
-    });
-  },
+        message0: Blockly.Msg['TEXT_PRINT_TITLE'],
+        args0: [
+          {
+            type: "input_value",
+            name: "TEXT"
+          }
+        ],
+        style: "text_blocks",
+        tooltip: Blockly.Msg['TEXT_PRINT_TOOLTIP']
+      });
+    },
   },
   generator: (block) => {
     // Print statement.
     var value_text = Blockly.Python.valueToCode(block, 'TEXT', Blockly.Python.ORDER_ATOMIC);
     //const code = 'print(' + msg + ')\n';
-    var code = 'print'+ value_text
+    var code = 'print' + value_text
     //return [code, Blockly.Python.ORDER_MEMBER];
     return code;
   },
 };
 
-const helloWorld =  {
+const helloWorld = {
   name: 'HelloWorld',
   category: 'Demo',
   block: {
@@ -59,65 +59,83 @@ const helloWorld =  {
   },
 };
 
-class App extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.handleChange = this.handleChange.bind(this);
-  //   this.handleClick = this.handleClick.bind(this);
-  //   this.state = {
-  //     pythonCode: ""
-  //   };
-  // }
-  //
-  // handleClick() {
-  //   console.log(this.state.pythonCode);
-  // }
-  //
-  // handleChange(code, workspace) {
-  //   this.setState({
-  //        pythonCode: code
-  //   });
-  // }
+// Extra Komponente nur für den Blockly-Teil
+// Dann kansnt du das shouldComponentUpdate here auf false setzen
+// (dh diese Komponente rendert *nie* neu)
+class BlocklyWrapper extends Component {
+  shouldComponentUpdate() {
+    return false;
+  }
 
-
-//onChange={this.handleChange} das würde ich gerne stattdessen unten in BlocklyDrawer nutzen in Verbindung mit dem auskommentierten Code oben. Aber dann funktioniert die Toolbox nicht mehr
   render() {
     return (
       <div>
         <BlocklyDrawer
-      language={Blockly.Python}
-      tools={[helloWorld, kNearNeigh]}
-      onChange={(code, workspace) => {
-        // das sollte ja auch eigentlich so gehen, führt aber zum selben Fehler wie das ganze in einer extra Funktion
-        //this.setState({ pythonCode: code});
-        console.log(code);
-      }}
-      appearance={
-        {
-          categories: {
-            Demo: {
-              colour: '270'
-            },
-          },
-        }
-      }
-    >
-      <Category name="Variables" custom="VARIABLE" />
-      <Category name="Basics">
-        <Block type="controls_if"/>
-        <Block type="logic_compare"/>
-        <Block type="controls_repeat_ext"/>
-        <Block type="math_arithmetic"/>
-        <Block type="text"/>
-        <Block type="text_print"/>
-        <Block type="math_number" />
-      </Category>
-      <Category name="Values">
-        <Block type="math_number" />
-        <Block type="text" />
-      </Category>
-    </BlocklyDrawer>
-    <Button onClick={this.handleClick}>Click me!</Button>
+          language={Blockly.Python}
+          tools={[helloWorld, kNearNeigh]}
+          onChange={this.props.handleChange}
+          appearance={
+            {
+              categories: {
+                Demo: {
+                  colour: '270'
+                },
+              },
+            }
+          }
+        >
+          <Category name="Variables" custom="VARIABLE" />
+          <Category name="Basics">
+            <Block type="controls_if" />
+            <Block type="logic_compare" />
+            <Block type="controls_repeat_ext" />
+            <Block type="math_arithmetic" />
+            <Block type="text" />
+            <Block type="text_print" />
+            <Block type="math_number" />
+          </Category>
+          <Category name="Values">
+            <Block type="math_number" />
+            <Block type="text" />
+          </Category>
+        </BlocklyDrawer>
+        <Button onClick={this.props.handleClick}>Click me!</Button>
+      </div>
+    );
+  }
+}
+
+// ...diese aber schon wenn sich der State oder Props ändern!
+class App extends Component {
+  // Du kannst den State auch so initialisieren, dann sparst du dir den
+  // Konstruktor und den super(props) Aufruf
+  state = {
+    pythonCode: '',
+  }
+
+  // ...und wenn du die Event handler wie folgt spezifizierst, brauchst du nicht das .bind(this)
+  // (ist quasi ein Klassenattribut, dem eine Arrow-Funktion zugewiesen wird. Die Arrow-Funktionen
+  //  behalten den this-Kontext des Elternelements bei und man muss dieses deswegen nicht binden)
+  handleClick = () => {
+    console.log(this.state);
+  }
+
+  handleChange = (code, workspace) => {
+    this.setState({
+      pythonCode: code,
+    });
+  }
+
+
+  //onChange={this.handleChange} das würde ich gerne stattdessen unten in BlocklyDrawer nutzen in Verbindung mit dem auskommentierten Code oben. Aber dann funktioniert die Toolbox nicht mehr
+  render() {
+    return (
+      <div>
+        <BlocklyWrapper handleChange={this.handleChange} handleClick={this.handleClick}  />
+
+        <pre>
+          {JSON.stringify(this.state, null, 4)}
+        </pre>
       </div>
     );
   }
